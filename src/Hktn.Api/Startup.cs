d828355cc8c996938103ee50412fc2f5180c0c2d
turgayozgur <ozgurtrgy@gmail.com>
2020-07-16 12:41:09 +0300

    AddHttpContextAccessor

--- src/Hktn.Api/Startup.cs
+++ src/Hktn.Api/Startup.cs
@@ -20,6 +20,8 @@ namespace Hktn.Api
             services.AddMemoryCache();
 
             services.AddHttpClient();
+
+            services.AddHttpContextAccessor();
             
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
